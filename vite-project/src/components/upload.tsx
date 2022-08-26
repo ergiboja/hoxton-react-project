4904e78b7f72eae1967dd702c3a82bfafb5ba99c
Ergi  Boja <perdoruesperdoruesi@gmail.com>
2022-08-26 14:31:17 +0200

    test

--- vite-project/src/components/upload.tsx
+++ vite-project/src/components/upload.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 
 
 function Upload() {
 
-  }
+
 
   return (
-   
+   <div className="uploadcont">
+    
+    <input type="file" className="form-control" placeholder="File"/><br />
+    <input type="text" className="form-control" placeholder="Name"/><br />
+    <button className="uploadbtn">Submit</button><NavLink to="/home">OR Go Back</NavLink>
+
+
+
+
+
+   </div>
   )
 }
 
